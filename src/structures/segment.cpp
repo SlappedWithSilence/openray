0d0a4f4229754f342d25e6b6cbe4bf17fb497916
Slapped with Silence <asigal55+02@gmail.com>
2022-06-21 16:29:33 -0500

    Added color struct, improved directional vector and vec3

--- src/structures/segment.cpp
+++ src/structures/segment.cpp
@@ -5,62 +5,90 @@
 #include "segment.h"
 #include "math/fast_math.h"
 
-/* vec3 definitions */
+#include <ostream>
+
+/*** vec3 definitions ***/
 openray::vec3::vec3(float x, float y, float z)
-    :dim({x, y, z})
+    : x(x), y(y), z(z)
 {}
 
-
 openray::vec3::vec3(const openray::vec3 &other)
-    : dim(other.dim)
+    : x(other.x), y(other.y), z(other.z)
 {}
 
 // Vector dot product
 openray::vec3 openray::vec3::dot(const openray::vec3 &other) {
-    float dot_x = *(this->x) * *(other.x);
-    float dot_y = *(this->y) * *(other.y);
-    float dot_z = *(this->z) * *(other.z);
+    float dot_x = x * other.x;
+    float dot_y = y * other.y;
+    float dot_z = z * other.z;
     return {dot_x, dot_y, dot_z};
 }
 
 // Vector cross product
 // Returns a vector with a 90-degree angle to both composite vectors (unit vector)
-openray::vec3 openray::vec3::cross(const openray::vec3 &other) {
-    float cross_x = *(this->y) * *(other.z) - *(this->z) * *(other.y);
-    float cross_y = *(this->z) * *(other.x) - *(this->x) * *(other.z);
-    float cross_z = *(this->x) * *(other.y) - *(this->y) * *(other.x);
+openray::vec3 openray::vec3::cross(const openray::vec3 &other) const {
+    float cross_x = y * other.z - z * other.y;
+    float cross_y = z * other.x - x * other.z;
+    float cross_z = x * other.y - y * other.x;
     return {cross_x, cross_y, cross_z};
 }
 
 // Add two vec3's together
 openray::vec3 openray::vec3::operator+(const openray::vec3 &other) {
-    return {};
+    return {x + other.x, y+other.y, z+other.z};
 }
 
 // Scale a vec3 by 'scalar'
-openray::vec3 openray::vec3::operator*(const int scalar) {
-    return openray::vec3({*(this->x) * (float) scalar,
-                          *(this->y) * (float) scalar,
-                          *(this->z) * (float) scalar
-                            }
-                          );
-}
-
-// Scale a vec3 by 'scalar'
-openray::vec3 openray::vec3::operator*(const float scalar) {
-    return openray::vec3({(*(this->x) * scalar),
-                          (*(this->y) * scalar),
-                          (*(this->z) * scalar)}
-    );
-}
-
-openray::vec3 openray::vec3::norm() {
-    float divisor = openray::inverse_sqrt(*(this->x) * *(this->x) +
-                                                 *(this->y) * *(this->y) +
-                                                 *(this->z) * *(this->z)
-                                                 );
-    return {(*(this->x) * divisor),
-            (*(this->y) * divisor),
-            (*(this->z) * divisor)
+openray::vec3 openray::vec3::operator*(const int scalar) const {
+    return {x * (float) scalar,
+            y * (float) scalar,
+            z * (float) scalar
             };
+}
+
+// Scale a vec3 by 'scalar'
+openray::vec3 openray::vec3::operator*(const float scalar) const {
+    return {x * scalar,
+            y * scalar,
+            z * scalar
+            };
+}
+
+// Return a normalized vector
+openray::vec3 openray::vec3::norm() const {
+    float divisor = openray::inverse_sqrt(x*x + y*y +z*z); // Calculate the divisor using an approximation
+    return {x * divisor,
+            y * divisor,
+            z * divisor
+            };
+}
+
+std::ostream &openray::operator<<(std::ostream &out, openray::vec3 &v) {
+    out << "<" << v.x << ", " << v.y << ", " << v.z << ">";
+    return out;
+}
+
+/*** Color Definitions ***/
+openray::Color::Color(int r, int g, int b)
+    : r(r), g(g), b(b)
+{}
+
+void openray::Color::lighten(openray::Color light_source_color) {
+
+}
+
+openray::Color openray::Color::operator+(const openray::Color &other) {
+    return openray::Color();
+}
+
+openray::Color openray::Color::operator+(int scalar) {
+    return openray::Color();
+}
+
+openray::Color openray::Color::operator*(int scalar) {
+    return openray::Color();
+}
+
+openray::Color openray::Color::operator*(float scalar) {
+    return openray::Color();
 }
--- src/structures/segment.h
+++ src/structures/segment.h
@@ -6,14 +6,13 @@
 #define OPENRAY_SEGMENT_H
 
 #include <vector>
+#include <string>
+
 namespace openray {
     struct vec3 {
-
-        // Members
-        std::vector<float> dim {0,0,0};
-        float* x = dim.data();
-        float* y = x + 1;
-        float* z = x + 1;
+        float x {0};
+        float y {0};
+        float z {0};
 
         // Constructors
         vec3() = default;
@@ -21,20 +20,42 @@ namespace openray {
         vec3(const vec3 &other);
 
         // Functions
-        vec3 dot(const vec3 &other);
-        vec3 cross(const vec3 &other);
-        vec3 norm();
+        [[nodiscard]] vec3 dot(const vec3 &other);
+        [[nodiscard]] vec3 cross(const vec3 &other) const;
+        [[nodiscard]] vec3 norm() const;
 
         // Operator overloads
         vec3 operator+ (const vec3 &other);
-        vec3 operator* (int scalar);
-        vec3 operator* (float scalar);
+        vec3 operator* (int scalar) const;
+        vec3 operator* (float scalar) const;
+        friend std::ostream& operator<<(std::ostream &out, vec3 &v);
 
     };
 
-    struct directional_vector {
+    struct DirectionalVector {
         vec3 origin {0,0,0};
         vec3 endpoint {1,1,1};
+    };
+
+    struct Color {
+        int r{0}, g{0}, b {0};
+
+        Color() = default;
+        Color(int r, int g, int b);
+
+        // Functions
+        void lighten(Color light_source_color);
+
+        // Operator overloads
+        Color operator+ (const Color &other);
+        Color operator+ (int scalar);
+        Color operator* (int scalar);
+        Color operator* (float scalar);
+    };
+
+    struct Ray: DirectionalVector {
+        int depth {0};
+        const int MAX_DEPTH = {5};
     };
 }
 
