f56b1eea03a2ffebe655b7c7b7eb8c00b9294d16
Slapped with Silence <asigal55+02@gmail.com>
2022-06-20 23:44:47 -0500

    Updated driver

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,16 @@
 // Created by Aaron on 6/20/2022.
 //
 
+#include "structures/segment.h"
+#include <iostream>
+
+using namespace openray;
+using namespace std;
+
 int main() {
+
+    vec3 v = vec3(0,1,0);
+    cout << *(v.x);
 
     return 1;
 }
