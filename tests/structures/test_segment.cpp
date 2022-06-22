9d29c393a34e143e080e6cab105e508e32c1cd8f
Slapped With Silence <asigal55+02@gmail.com>
2022-06-22 15:57:37 -0500

    Added test cases

--- tests/structures/test_segment.cpp
+++ tests/structures/test_segment.cpp
@@ -15,6 +15,24 @@ TEST(vec3Test, Initialize) {
     EXPECT_EQ(v.x == -1 && v.y == -3 && v.z == 2, true); // Did the brace initializer work?
 }
 
+TEST(vec3Test, EqualitySelf) {
+    vec3 v0 = vec3();
+    vec3 v1 = vec3(1,1,1);
+
+    EXPECT_EQ(v0 == v0, true);
+    EXPECT_EQ(v1 == v1, true);
+}
+
+TEST(vec3Test, EqualityOther) {
+    vec3 v0, v0_other = vec3();
+    vec3 v1 = vec3(1,1,1);
+    vec3 v1_other = vec3(1,1,1);
+
+    EXPECT_EQ(v1 == v1_other, true);
+    EXPECT_EQ(v0 == v1, false);
+    EXPECT_EQ(v0, v0_other);
+}
+
 TEST(vec3Test, AddTwoVec3s) {
     vec3 v0 = vec3();
     vec3 v1 = {1, 1, 1};
@@ -24,4 +42,14 @@ TEST(vec3Test, AddTwoVec3s) {
     EXPECT_EQ(v_sum.x == 1 && v_sum.y == 1 && v_sum.z == 1, true);
     v_sum = v1 + v2;
     EXPECT_EQ(v_sum.x == (v1.x + v2.x) && v_sum.y == (v1.y + v2.y) && v_sum.z == (v1.z + v2.z), true);
+}
+
+TEST(vec3Test, SubtractTwoVec3s) {
+    vec3 v0 = vec3();
+    vec3 v1 = vec3(1,1,1);
+    vec3 v2 = vec3(2, 6, -4);
+
+    vec3 v_diff = v0 - v1;
+    EXPECT_EQ(v_diff, vec3(-1,-1,-1));
+    EXPECT_EQ(v0-v2, vec3(-2, -6, 4));
 }
