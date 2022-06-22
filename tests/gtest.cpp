c04ea9893f72c84c503bca99b28060a2e3df6a29
Slapped with Silence <asigal55+02@gmail.com>
2022-06-21 20:13:49 -0500

    Adjusted CMake structure to accommodate G-Test

---
+++ tests/gtest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Aaron on 6/21/2022.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
